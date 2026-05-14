1b442821a804a583c6ca2af1d7fd974155332602
ChaosCantrip <dev@chaoscantrip.com>
2026-05-14 21:19:56 +0100

    Added basic custom ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,20 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "brace-style": ["warn", "allman"],
+      "quotes": ["warn", "double"],
+      "indent": ["warn", 4, { "SwitchCase": 1 }],
+      "eol-last": ["warn", "always"]
+    }
+  },
+  {
+    files: ["**/*.{mjs,mts}"],
+    rules: {
+      "indent": ["warn", 2, { "SwitchCase": 1 }]
+    }
+  }
 ]);
 
 export default eslintConfig;
