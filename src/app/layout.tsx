9ae003ddf46890e163bc9dc2f6610aa3a37a7cfc
ChaosCantrip <dev@chaoscantrip.com>
2026-05-27 00:34:19 +0100

    Added Title Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import "@/styles/global/master.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "RaDdle",
+    description: "RaDdle by ChaosCantrip",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) 
 {
