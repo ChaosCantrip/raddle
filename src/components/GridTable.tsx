323e962e6d70d334b393f055515bb8d627f2171d
ChaosCantrip <dev@chaoscantrip.com>
2026-05-27 22:10:33 +0100

    We do a little styling

--- src/components/GridTable.tsx
+++ src/components/GridTable.tsx
@@ -4,6 +4,14 @@ export default function GridTable({ children }: { children: React.ReactNode })
 {
     return (
         <div className={styles.gridTable}>
+            <div className={styles.columnHeaders}>
+                <div className={styles.columnHeader}>Encounter</div>
+                <div className={styles.columnHeader}>Raid or Dungeon</div>
+                <div className={styles.columnHeader}>Activity</div>
+                <div className={styles.columnHeader}>Enemy Type(s)</div>
+                <div className={styles.columnHeader}>Encounter Number</div>
+                <div className={styles.columnHeader}>Expansion</div>
+            </div>
             {children}
         </div>
     );
