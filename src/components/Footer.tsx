a03ef42d04efa60cd6a60258e750f9fba73e20b4
James Donald <jdonald@bpops.co.uk>
2026-05-28 11:27:11 +0100

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Footer.module.css";
 
 export default function Footer()
 {
     return (
         <footer className={styles.footer}>
-            <p className={styles.version_number}>RaDdle v{process.env.NEXT_PUBLIC_VERSION_NUMBER}</p>
-            <p>by ChaosCantrip</p>
+            <p><a href={"https://github.com/ChaosCantrip/raddle"} target="_blank"rel="noopener noreferrer"><GitHubIcon /> RaDdle</a> v{process.env.NEXT_PUBLIC_VERSION_NUMBER}</p>
+            <p>by <a href={"https://chaoscantrip.com"} target="_blank"rel="noopener noreferrer"><UserIcon /> ChaosCantrip</a></p>
         </footer>
+    )
+}
+
+function GitHubIcon()
+{
+    return (
+        <FontAwesomeIcon icon={faGithub} className={styles.icon + " " + styles.github_icon} />
+    )
+}
+
+function UserIcon()
+{
+    return (
+        <FontAwesomeIcon icon={faUser} className={styles.icon + " " + styles.user_icon} />
     )
 }
