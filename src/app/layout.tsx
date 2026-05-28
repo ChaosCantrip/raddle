3aa1f2fd1fab56b4d107afa140bf01425132b893
James Donald <jdonald@bpops.co.uk>
2026-05-28 11:15:43 +0100

    Font Awesome Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import "@/styles/global/master.css";
 import { Metadata } from "next";
 
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false;
+
 export const metadata: Metadata = {
     title: "RaDdle",
     description: "RaDdle by ChaosCantrip",
