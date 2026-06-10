5d6058f03ef60316405e54429f4ebc7e512ff2ab
ChaosCantrip <dev@chaoscantrip.com>
2026-06-10 09:36:10 +0100

    Changed Home Buttons into Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
@@ -11,9 +12,13 @@ export default function HomePage()
             <div className={styles.introduction}>
                 <p className={styles.mainText}>Welcome to RaDdle!</p>
                 <p className={styles.subText}>Play <span className={styles.daily}>Daily</span> to guess today&apos;s Raid or Dungeon Encounter!</p>
-                <a href="/daily" className={styles.button + " " + styles.dailyLink}>Play Daily <CaretRightIcon /></a>
+                <Link href="/daily" className={styles.button + " " + styles.dailyLink}>
+                    Play Daily <CaretRightIcon />
+                </Link>
                 <p className={styles.subText}>Or play <span className={styles.arcade}>Arcade</span> to guess random Encounters to your heart&apos;s content!</p>
-                <a href="/arcade" className={styles.button + " " + styles.arcadeLink}>Play Arcade <CaretRightIcon /></a>
+                <Link href="/arcade" className={styles.button + " " + styles.arcadeLink}>
+                    Play Arcade <CaretRightIcon />
+                </Link>
             </div>
         </div>
     );
