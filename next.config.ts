834f8b40b4fb42f3a7a4eb4bf9b8ccdb2fcd2564
ChaosCantrip <dev@chaoscantrip.com>
2026-06-10 09:36:05 +0100

    Added Version Number to bottom of page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+import packageJson from "./package.json";
+
 const nextConfig: NextConfig = {
-    "output": "standalone"
+    output: "standalone",
+    env: {
+        NEXT_PUBLIC_VERSION_NUMBER: packageJson.version
+    }
 };
 
 export default nextConfig;
