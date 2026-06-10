405ba20688d3c3f44f039f1ec6e343d44dcd75eb
ChaosCantrip <dev@chaoscantrip.com>
2026-06-10 09:36:14 +0100

    Added eager loading to banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header()
     return (
         <header className={styles.header}>
             <Link href="/" className={styles.link}>
-                <Image src={BannerImage} className={styles.banner} alt="RaDdle"/>
+                <Image src={BannerImage} className={styles.banner} alt="RaDdle" loading="eager"/>
             </Link>
         </header>
     )
