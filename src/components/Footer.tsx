ea97358be071389bf4abf813073dd3ddb1ee1b4d
ChaosCantrip <dev@chaoscantrip.com>
2026-06-10 09:36:14 +0100

    Updated Footer credits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Footer.module.css";
 
@@ -8,8 +7,8 @@ export default function Footer()
 {
     return (
         <footer className={styles.footer}>
-            <p><a href={"https://github.com/ChaosCantrip/raddle"} target="_blank"rel="noopener noreferrer"><GitHubIcon /> RaDdle</a> v{process.env.NEXT_PUBLIC_VERSION_NUMBER}</p>
-            <p>by <a href={"https://chaoscantrip.com"} target="_blank"rel="noopener noreferrer"><UserIcon /> ChaosCantrip</a></p>
+            <p><a href={"https://github.com/ChaosCantrip/raddle"} target="_blank"rel="noopener noreferrer"><GitHubIcon /> RaDdle</a> v{process.env.NEXT_PUBLIC_VERSION_NUMBER} by <a href={"https://chaoscantrip.com"} target="_blank"rel="noopener noreferrer">ChaosCantrip</a></p>
+            <p>Logo & Banner by <a href={"https://x.com/LukeCB14"} target="_blank"rel="noopener noreferrer">LukeCB14</a></p>
         </footer>
     )
 }
@@ -18,12 +17,5 @@ function GitHubIcon()
 {
     return (
         <FontAwesomeIcon icon={faGithub} className={styles.icon + " " + styles.github_icon} />
-    )
-}
-
-function UserIcon()
-{
-    return (
-        <FontAwesomeIcon icon={faUser} className={styles.icon + " " + styles.user_icon} />
     )
 }
