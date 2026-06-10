8ad7a041d36356b688dcbd2e0ec86e9d7eb9d60b
ChaosCantrip <dev@chaoscantrip.com>
2026-06-10 09:36:06 +0100

    Moved FilterEncounters into Utils

---
+++ src/app/lib/Utils/FilterEncounters.ts
@@ -0,0 +1,13 @@
+import Encounter from "@/app/models/Encounter";
+
+function FilterEncounters(inputValue: string, encounters: Encounter[], guesses: Encounter[]) 
+{
+    const query = inputValue.toLowerCase();
+
+    const startsWithMatches = encounters.filter(encounter => encounter.name.toLowerCase().startsWith(query) && !guesses.includes(encounter));
+    const includesMatches = encounters.filter(encounter => encounter.name.toLowerCase().includes(query) && !startsWithMatches.includes(encounter));
+
+    return [...startsWithMatches, ...includesMatches];
+}
+
+export default FilterEncounters;
