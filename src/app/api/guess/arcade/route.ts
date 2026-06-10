9398b508270429dae1a17e0930ac544da58dfc6e
ChaosCantrip <dev@chaoscantrip.com>
2026-06-10 09:36:10 +0100

    Moved Encounter Comparison Logic to lib/CompareEncounters

--- src/app/api/guess/arcade/route.ts
+++ src/app/api/guess/arcade/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import Encounters from "@/lib/Encounters";
-import GridCellState from "@/models/GridCellState";
+import CompareEncounters from "@/lib/CompareEncounters";
 
 export async function POST(request: NextRequest) 
 {
@@ -37,100 +37,11 @@ export async function POST(request: NextRequest)
         return NextResponse.json({ result: "correct" });
     }
 
-    const name_result = CompareName(guess.name, answer.name);
-    const enemy_types_result = CompareEnemyTypes(guess.enemy_types, answer.enemy_types);
-    const activity_type_result = CompareActivityType(guess.activity_type, answer.activity_type);
-    const activity_result = CompareActivity(guess.activity, answer.activity);
-    const expansion_result = CompareExpansion(guess.expansion, answer.expansion);
-    const encounters_result = CompareEncounters(guess.encounters, answer.encounters);
+    const result = CompareEncounters(guess, answer);
 
     return NextResponse.json({
         result: "incorrect",
-        comparisons: {
-            name: name_result,
-            enemy_types: enemy_types_result,
-            activity_type: activity_type_result,
-            activity: activity_result,
-            expansion: expansion_result,
-            encounters: encounters_result
-        }
+        comparisons: result
     });
 }
-
-function CompareName(guess: string, answer: string): GridCellState
-{
-    // Added for future proofing. I don't know how this would ever change, but... Ah well.
-    if (guess === answer) 
-    {
-        return GridCellState.Green;
-    }
     
-    return GridCellState.Grey;
-}
-
-function CompareEnemyTypes(guess: string[], answer: string[]): GridCellState
-{
-    const guessSet = new Set(guess);
-    const answerSet = new Set(answer);
-    const intersection = new Set([...guessSet].filter(x => answerSet.has(x)));
-
-    if (intersection.size === answerSet.size && intersection.size === guessSet.size) 
-    {
-        return GridCellState.Green;
-    } 
-    else if (intersection.size > 0) 
-    {
-        return GridCellState.Yellow;
-    }
-
-    return GridCellState.Grey;
-}
-
-function CompareActivityType(guess: string, answer: string): GridCellState
-{
-    // Added for future proofing. I don't know how this would ever change, but... Ah well.
-    if (guess === answer) 
-    {
-        return GridCellState.Green;
-    }
-
-    return GridCellState.Grey;
-}
-
-function CompareActivity(guess: string, answer: string): GridCellState
-{
-    // Added for future proofing. I don't know how this would ever change, but... Ah well.
-    if (guess === answer) 
-    {
-        return GridCellState.Green;
-    }
-
-    return GridCellState.Grey;
-}
-
-function CompareExpansion(guess: string, answer: string): GridCellState 
-{
-    // Added for future proofing. I don't know how this would ever change, but... Ah well.
-    
-    if (guess === answer) 
-    {
-        return GridCellState.Green;
-    }
-
-    return GridCellState.Grey;
-
-}
-
-function CompareEncounters(guess: number[], answer: number[]): GridCellState 
-{
-    const guessSet = new Set(guess);
-    const answerSet = new Set(answer);
-    const intersection = new Set([...guessSet].filter(x => answerSet.has(x)));
-
-    if (intersection.size > 0) 
-    {
-        return GridCellState.Green;
-    }
-
-    return GridCellState.Grey;
-}
--- src/app/api/guess/daily/route.ts
+++ src/app/api/guess/daily/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Encounters from "@//lib/Encounters";
 import GetDailyAnswer from "@/lib/DailyAnswer";
-import GridCellState from "@/models/GridCellState";
+import CompareEncounters from "@/lib/CompareEncounters";
 
 export async function POST(request: NextRequest) 
 {
@@ -37,100 +37,10 @@ export async function POST(request: NextRequest)
         return NextResponse.json({ result: "correct" });
     }
 
-    const name_result = CompareName(guess.name, todays_answer.name);
-    const enemy_types_result = CompareEnemyTypes(guess.enemy_types, todays_answer.enemy_types);
-    const activity_type_result = CompareActivityType(guess.activity_type, todays_answer.activity_type);
-    const activity_result = CompareActivity(guess.activity, todays_answer.activity);
-    const expansion_result = CompareExpansion(guess.expansion, todays_answer.expansion);
-    const encounters_result = CompareEncounters(guess.encounters, todays_answer.encounters);
+    const result = CompareEncounters(guess, todays_answer);
 
     return NextResponse.json({
         result: "incorrect",
-        comparisons: {
-            name: name_result,
-            enemy_types: enemy_types_result,
-            activity_type: activity_type_result,
-            activity: activity_result,
-            expansion: expansion_result,
-            encounters: encounters_result
-        }
+        comparisons: result
     });
-}
-
-function CompareName(guess: string, answer: string): GridCellState
-{
-    // Added for future proofing. I don't know how this would ever change, but... Ah well.
-    if (guess === answer) 
-    {
-        return GridCellState.Green;
-    }
-    
-    return GridCellState.Grey;
-}
-
-function CompareEnemyTypes(guess: string[], answer: string[]): GridCellState
-{
-    const guessSet = new Set(guess);
-    const answerSet = new Set(answer);
-    const intersection = new Set([...guessSet].filter(x => answerSet.has(x)));
-
-    if (intersection.size === answerSet.size && intersection.size === guessSet.size) 
-    {
-        return GridCellState.Green;
-    } 
-    else if (intersection.size > 0) 
-    {
-        return GridCellState.Yellow;
-    }
-
-    return GridCellState.Grey;
-}
-
-function CompareActivityType(guess: string, answer: string): GridCellState
-{
-    // Added for future proofing. I don't know how this would ever change, but... Ah well.
-    if (guess === answer) 
-    {
-        return GridCellState.Green;
-    }
-
-    return GridCellState.Grey;
-}
-
-function CompareActivity(guess: string, answer: string): GridCellState
-{
-    // Added for future proofing. I don't know how this would ever change, but... Ah well.
-    if (guess === answer) 
-    {
-        return GridCellState.Green;
-    }
-
-    return GridCellState.Grey;
-}
-
-function CompareExpansion(guess: string, answer: string): GridCellState 
-{
-    // Added for future proofing. I don't know how this would ever change, but... Ah well.
-    
-    if (guess === answer) 
-    {
-        return GridCellState.Green;
-    }
-
-    return GridCellState.Grey;
-
-}
-
-function CompareEncounters(guess: number[], answer: number[]): GridCellState 
-{
-    const guessSet = new Set(guess);
-    const answerSet = new Set(answer);
-    const intersection = new Set([...guessSet].filter(x => answerSet.has(x)));
-
-    if (intersection.size > 0) 
-    {
-        return GridCellState.Green;
-    }
-
-    return GridCellState.Grey;
 }
