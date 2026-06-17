e516f77a870a30230f6c9c72194b832fd52d10a7
ChaosCantrip <dev@chaoscantrip.com>
2026-06-17 13:19:32 +0100

    Added ESLint config to @raddle/types package

---
+++ packages/types/eslint.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "eslint/config";
+import { baseESLintConfig } from "@raddle/config/eslint";
+
+const eslintConfig = defineConfig([
+  ...baseESLintConfig
+]);
+
+export default eslintConfig;
