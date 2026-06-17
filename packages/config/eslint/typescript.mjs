1305e9a3bb45911a7679bc8aaa89130f9aea8803
ChaosCantrip <dev@chaoscantrip.com>
2026-06-17 13:15:52 +0100

    Updated ESLint config to cover typescript

---
+++ packages/config/eslint/typescript.mjs
@@ -0,0 +1,30 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export const typescript = [
+    {
+      files: ["**/*.{ts,tsx}"],
+
+      languageOptions: {
+        parser: tsParser,
+        parserOptions: {
+          sourceType: "module",
+          ecmaVersion: "latest"
+        }
+      },
+
+      plugins: {
+        "@typescript-eslint": tsPlugin
+      },
+
+      rules: {
+        ...tsPlugin.configs.recommended.rules,
+        "@typescript-eslint/consistent-type-imports": [
+            "warn",
+            {
+                prefer: "type-imports"
+            }
+        ]
+      }
+    }
+  ];
