86fdb1de3715da0963164e31ae2c943ab238f10e
ChaosCantrip <dev@chaoscantrip.com>
2026-06-23 09:55:54 +0100

    Added cookie-parser to api

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { router } from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 export const createApp = () => 
 {
     const app = express();
 
     app.use(express.json());
+    app.use(cookieParser());
     app.use("/api", router);
 
     return app;
