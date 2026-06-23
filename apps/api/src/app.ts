9f221b9ddd8aa795d97bdacc373c064a74c47ce1
ChaosCantrip <dev@chaoscantrip.com>
2026-06-23 10:01:43 +0100

    Added LogRequest middleware

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import { router } from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import { LogRequest } from "./middlewares/index.js";
 
 export const createApp = () => 
 {
     const app = express();
 
+    // Standard middleware
     app.use(express.json());
     app.use(cookieParser());
+
+    // Custom middleware
+    app.use(LogRequest);
+
+    // Routes
     app.use("/api", router);
 
     return app;
