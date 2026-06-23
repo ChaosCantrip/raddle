a41a0c947b256710e995df8d6ab673040ab62974
ChaosCantrip <dev@chaoscantrip.com>
2026-06-23 10:05:29 +0100

    Added mongodb to api

---
+++ apps/api/src/lib/mongodb.ts
@@ -0,0 +1,32 @@
+import { MongoClient } from "mongodb";
+
+let clientPromise: Promise<MongoClient>;
+
+async function GetMongoClient(): Promise<MongoClient> 
+{
+    if (!clientPromise) 
+    {
+        const MONGODB = {
+            USER: process.env.MONGODB_USER,
+            PASS: process.env.MONGODB_PASS,
+            HOST: process.env.MONGODB_HOST,
+            PORT: process.env.MONGODB_PORT
+        }
+
+        if (!MONGODB.USER || !MONGODB.PASS || !MONGODB.HOST || !MONGODB.PORT) 
+        {
+            throw new Error("Invalid MONGODB Environment Variables!");
+        }
+
+        const uri = `mongodb://${MONGODB.USER}:${MONGODB.PASS}@${MONGODB.HOST}:${MONGODB.PORT}/`
+
+        const options = {};
+
+        const client = new MongoClient(uri, options);
+        clientPromise = client.connect();
+    }
+
+    return clientPromise;
+}
+
+export { GetMongoClient };
