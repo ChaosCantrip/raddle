e5d693d1de18a9df0cb6cddbbdc8b53fd4c4ed89
ChaosCantrip <dev@chaoscantrip.com>
2026-06-26 10:39:30 +0100

    Added /games/guess router

--- apps/api/src/routes/games/index.ts
+++ apps/api/src/routes/games/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import type { Router } from "express";
 
+import { setupGuessRouter } from "./guess/index.js";
+
 const gamesRouter: Router = express.Router();
+
+setupGuessRouter(gamesRouter);
 
 export function setupGamesRouter(router: Router)
 {
