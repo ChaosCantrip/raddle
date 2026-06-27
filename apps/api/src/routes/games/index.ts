e0b7983315f04bd00e9c00cd580f61b55db43642
ChaosCantrip <dev@chaoscantrip.com>
2026-06-27 17:32:55 +0100

    Added /games/:gameId route to @raddle/api

--- apps/api/src/routes/games/index.ts
+++ apps/api/src/routes/games/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import type { Router } from "express";
 
+import { setupGetGameEndpoint } from "./get.js";
 import { setupGuessRouter } from "./guess/index.js";
 
 const gamesRouter: Router = express.Router();
 
 setupGuessRouter(gamesRouter);
+setupGetGameEndpoint(gamesRouter);
 
 export function setupGamesRouter(router: Router)
 {
