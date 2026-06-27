8c5c18a255cac87ea51da9840a0791e80287c3ab
ChaosCantrip <dev@chaoscantrip.com>
2026-06-27 20:51:14 +0100

    Added reactive Login with Bungie button

--- apps/website/src/components/Header.tsx
+++ apps/website/src/components/Header.tsx
@@ -3,15 +3,22 @@ import Link from "next/link";
 
 import BannerImage from "@/../public/banner.webp";
 
+import BungieLoginMenu from "./BungieLoginMenu";
+
 import styles from "./Header.module.css";
 
-export default function Header()
+export default async function Header()
 {
+    // Placeholder sessions
+    // const session = { bungieName: "TestUser" };
+    const session = null
+
     return (
         <header className={styles.header}>
             <Link href="/" className={styles.link}>
                 <Image src={BannerImage} className={styles.banner} alt="RaDdle" loading="eager"/>
             </Link>
+            <BungieLoginMenu session={session} />
         </header>
     )
 }
