b48acd0f78083b3558a98350dab065389b12d4cc
ChaosCantrip <dev@chaoscantrip.com>
2026-06-27 16:09:50 +0100

    Removed unused components from @raddle/website/lib

--- apps/website/src/lib/GenerateRandomEncounter.ts
+++
@@ -1,7 +0,0 @@
-import type { Encounter } from "@raddle/types";
-import { Encounters } from "@raddle/common";
-
-export default function GenerateRandomEncounter(): Encounter
-{
-    return Encounters[Math.floor(Math.random() * Encounters.length)];
-}
--- apps/website/src/lib/index.ts
+++ apps/website/src/lib/index.ts
@@ -1,3 +1,1 @@
 export { default as Utils } from "./Utils";
-export { default as CompareEncounters } from "./CompareEncounters";
-export { default as GenerateRandomEncounter } from "./GenerateRandomEncounter";
