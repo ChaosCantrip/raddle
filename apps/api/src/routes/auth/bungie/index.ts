bb605f6b6df3f28195fd1d5ac60e40844d106853
ChaosCantrip <dev@chaoscantrip.com>
2026-06-27 20:39:16 +0100

    Added /auth/bungie/start router to @raddle/api

--- apps/api/src/routes/auth/bungie/index.ts
+++ apps/api/src/routes/auth/bungie/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import type { Router } from "express";
 
+import { setupStartRouter } from "./start/index.js";
+
 const bungieRouter: Router = express.Router();
+
+setupStartRouter(bungieRouter);
 
 export function setupBungieRouter(router: Router)
 {
