0612c3e7451e82c4e942bf51edcb849d7c18cc22
ChaosCantrip <dev@chaoscantrip.com>
2026-07-02 12:29:23 +0100

    Removed zod schema from User type

--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -1,12 +1,6 @@
-import { z } from "zod";
-
-export const membershipIdSchema = z.uuid();
-
-export const userSchema = z.object({
-    membershipId: membershipIdSchema,
-    displayName: z.string(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
-});
-
-export type User = z.infer<typeof userSchema>;
+export type User = {
+    membershipId: string;
+    displayName: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
